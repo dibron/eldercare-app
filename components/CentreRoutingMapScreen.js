47a910ab56a0c3d1742ccf3251242764b95fbaff
dibyansh <dibyanshshekhardas@gmail.com>
2024-05-22 18:32:09 +0530

    d

--- components/CentreRoutingMapScreen.js
+++ components/CentreRoutingMapScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const CentreRoutingMapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Centre Routing Map Screen</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* Add markers or other map elements here */}
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title={'Marker Title'}
+          description={'Marker Description'}
+        />
+      </MapView>
     </View>
   );
 }
 
 export default CentreRoutingMapScreen;
-
